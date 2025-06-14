4aa219b36b2af99dd5e49af378a1c7af0ff18133
LYASC <ariellayolla@gmail.com>
2025-06-14 20:23:10 +0200

    fix: intégration du JwtModule dans ProduitsModule pour corriger le JwtGuard et sécuriser les routes

--- backend/src/produits/produits.module.ts
+++ backend/src/produits/produits.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Produit } from './produit.entity';
 import { ProduitsService } from './produits.service';
 import { ProduitsController } from './produits.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Produit])],
+  imports: [TypeOrmModule.forFeature([Produit]), JwtModule],
   controllers: [ProduitsController],
   providers: [ProduitsService],
 })
